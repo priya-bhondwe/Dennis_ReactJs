62ba4f3a4f6c6d481a0cb7437ef638389b269d9f
priya-bhondwe <149312604+priya-bhondwe@users.noreply.github.com>
2024-03-21 16:18:10 +0530

    remove effect unused

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React, {useEffect} from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './assets/css/tailwind.css'
-import './assets/css/materialdesignicons.min.css'
-import Index from './main/index';
-import Switcher from './components/Switcher';
-import IndexTwo from './main/index-two';
-import IndexThree from './main/index-three';
-import IndexDark from './main/index-dark';
-import IndexRtl from './main/index-rtl';
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./assets/css/tailwind.css";
+import "./assets/css/materialdesignicons.min.css";
+import Index from "./main/index";
+import Switcher from "./components/Switcher";
+import IndexTwo from "./main/index-two";
+import IndexThree from "./main/index-three";
+import IndexDark from "./main/index-dark";
+import IndexRtl from "./main/index-rtl";
 
 function App() {
- 
   return (
-    <BrowserRouter >
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/index-two" element={<IndexTwo/>} />
-      <Route path="/index-three" element={<IndexThree/>} />
-      <Route path="/index-dark" element={<IndexDark/>} />
-      <Route path="/index-rtl" element={<IndexRtl/>} />
-    </Routes>
-    <Switcher/>
-  </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/index-two" element={<IndexTwo />} />
+        <Route path="/index-three" element={<IndexThree />} />
+        <Route path="/index-dark" element={<IndexDark />} />
+        <Route path="/index-rtl" element={<IndexRtl />} />
+      </Routes>
+      <Switcher />
+    </BrowserRouter>
   );
 }
 
